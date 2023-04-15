9fe0eeade6b3bc7e1aa24f7d5dc9d4dbc6a3f8bc
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-04-14 17:57:03 -0700

    fff

--- builtin_functions.c
+++ builtin_functions.c
@@ -131,27 +131,8 @@ int hsh_env(char **args, char *input_stdin, int *exit_status)
  * Return: exit
  */
 
-int hsh_exit(char **args, char *input_stdin, int *exit_status)
-{
-	int output_exit = 0;
+int myFunction() {
+	/*Perfomr Computations here*/
 
-	(void)args;
-	(void)input_stdin;
-	(void)exit_status;
-
-	if (args[1] == NULL)
-	{
-		free(args);
-		free(input_stdin);
-		exit(*exit_status);
-	}
-	if (args[2] != NULL)
-	{
-		/*may need write command*/
-		return (0);
-	}
-	output_exit = atoi(args[1]);
-	free(args);
-	free(input_stdin);
-	exit(output_exit);
+	return 2;
 }
